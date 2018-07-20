1e45fe123280f97a95e4a1857a3adac39fbe0440
Xiao Han <xiao.han@orange.com>
2018-07-20 15:43:45 +0200

    ask freertos to resume its sub-partition

--- pipcore-mp/src/partitions/x86/multiplexer/isr.c
+++ pipcore-mp/src/partitions/x86/multiplexer/isr.c
@@ -1,42 +1,44 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include <stdint.h>
-#include <pip/paging.h>
 #include <pip/api.h>
 #include <pip/vidt.h>
-#include <pip/compat.h>
-#include <pip/fpinfo.h>
+
+extern void *partitionEntry;
+extern void *securePartitionEntry;
+
+uint32_t isr_stack = NULL, test_page = NULL;
 
 INTERRUPT_HANDLER(timerRoutineAsm,timerRoutine)
-// printf("timerRoutine\r\n");
-// switch (data1) {
-// case queueCreate:
-//       queueCreateService(data2);
-//   break;
-// case queueSend:
-//   queueSendService(data2);
-//   break;
-// case queueReceive:
-//   queueReceiveService(data2);
-//   break;
-// case sbrk:
-//   sbrkService(data2);
-//   break;
-// case channelCom:
-//     channelService(data2);
-//     break;
-// default:
-//   __asm__ volatile("call vPortTimerHandler");
-// }
-//   //__asm__ volatile("call vPortTimerHandler");
-END_OF_INTERRUPT
+Pip_VCLI();
+schedule(caller);
+// printf("data1=0x%x, data2=0x%x, caller=0x%x\r\n", data1, data2, caller);
+if (data1 == 1){
+    if(!Pip_AddVAddr(test_page, partitionEntry, caller, 0x1, 0x1, 0x1)) {
+        printf("Ask FreeRTOS to resume a sub-partition: %x\r\n", caller);
+        Pip_Notify(partitionEntry, 0x81, caller, 0);
+    } else{
+        printf("unknown sub-partition: 0x%x\r\n", caller);
+        Pip_RemoveVAddr(partitionEntry, caller);
+    }
+}
+Pip_Resume(caller, 0);
+}
 
-INTERRUPT_HANDLER(spuriousIrqRoutineAsm,spuriousIrqRoutine)
+// INTERRUPT_HANDLER(spuriousIrqRoutineAsm,spuriousIrqRoutine)
+// Pip_VCLI();
 // printf("spuriousIrqRoutine, caller is 0x%x\r\n", caller);
-END_OF_INTERRUPT
+// END_OF_INTERRUPT
 
 void init_isr()
 {
-    registerInterrupt(33, &timerRoutineAsm, (uint32_t*)0xf2d0000 );
-    registerInterrupt(40, &spuriousIrqRoutineAsm, (uint32_t*) 0xf2d0000);
+    if (isr_stack == NULL){
+        isr_stack = Pip_AllocPage() + 0x1000 -4 ;
+    }
+    if (test_page == NULL){
+        test_page = Pip_AllocPage();
+    }
+    //32
+    Pip_RegisterInterrupt(33, &timerRoutineAsm, isr_stack );
+    // registerInterrupt(40, &spuriousIrqRoutineAsm, isr_stack);
 }
