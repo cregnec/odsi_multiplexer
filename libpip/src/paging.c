4a0ad594965120bec9390db9d8bc61398b0ef755
Xiao Han <xiao.han@orange.com>
2018-07-27 09:48:03 +0200

    count available pages

--- libpip/src/paging.c
+++ libpip/src/paging.c
@@ -6,6 +6,7 @@
 #define NULL 0
 
 void *Pager_FirstFreePage = NULL;
+static uint32_t nb_free_pages = 0;
 
 /* Initializes paging, depending on the addresses given */
 int Pip_InitPaging(void *begin, void *end)
@@ -28,6 +29,7 @@ int Pip_InitPaging(void *begin, void *end)
 		c += 1;
 	}
 	*(void**)b = (void*)0;
+    nb_free_pages = c;
 
     Pip_Debug_Puts("LibPip2 : Paging initialization complete. ");
     Pip_Debug_PutDec(c);
@@ -39,17 +41,23 @@ int Pip_InitPaging(void *begin, void *end)
 /* Allocates a page */
 void* Pip_AllocPage(void)
 {
+    if (nb_free_pages <=0){
+        Pip_Debug_Puts("LibPip2: no more free pages available\r\n");
+    }
     void* ret = Pager_FirstFreePage;
 		if(!ret)
 			return 0;
 
 		Pager_FirstFreePage = *(void**)ret;
+        nb_free_pages --;
 
-		
 		int j;
 		for(j=0;j<PGSIZE;j++)
 				((char*)ret)[j] = (char)0;
 
+    // Pip_Debug_Puts("LibPip2: alloc page: 0x");
+    // Pip_Debug_PutHex((unsigned long) ret);
+    // Pip_Debug_Puts("\r\n");
     return ret;
 }
 
@@ -58,4 +66,5 @@ void Pip_FreePage(void* page)
 {
     *(void**)page = Pager_FirstFreePage;
     Pager_FirstFreePage = page;
+    nb_free_pages ++;
 }
